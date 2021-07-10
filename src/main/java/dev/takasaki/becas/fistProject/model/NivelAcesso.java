14ddde94153c4998b8000afd59cc8cc0037d505c
Takasakiii <lucasmc2709@live.com>
2021-07-10 15:41:43 -0300

    NivelAcesso model

---
+++ src/main/java/dev/takasaki/becas/fistProject/model/NivelAcesso.java
@@ -0,0 +1,11 @@
+package dev.takasaki.becas.fistProject.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class NivelAcesso {
+    private final Long id;
+    private String descricao;
+}
