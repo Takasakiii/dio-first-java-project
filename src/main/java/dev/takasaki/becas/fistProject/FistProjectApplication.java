36ec7f7f7407a712bbad4967972b0fbf0bdfb515
Takasakiii <lucasmc2709@live.com>
2021-07-10 11:24:30 -0300

    Inicial Commit

---
+++ src/main/java/dev/takasaki/becas/fistProject/FistProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.takasaki.becas.fistProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FistProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/takasaki/becas/fistProject/FistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.takasaki.becas.fistProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FistProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
