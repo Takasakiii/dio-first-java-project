56adba56dcdd62d53f943c9be9ae385c7c94e5df
Takasakiii <lucasmc2709@live.com>
2021-07-14 14:00:47 -0300

    Finalizando JornadaTrabalho

--- src/main/java/dev/takasaki/becas/fistProject/controller/JornadaTrabalhoController.java
+++ src/main/java/dev/takasaki/becas/fistProject/controller/JornadaTrabalhoController.java
@@ -3,10 +3,11 @@ package dev.takasaki.becas.fistProject.controller;
 import dev.takasaki.becas.fistProject.model.JornadaTrabalho;
 import dev.takasaki.becas.fistProject.service.JornadaTrabalhoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/jornada")
@@ -21,5 +22,30 @@ public class JornadaTrabalhoController {
     @PostMapping
     public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho) {
         return service.save(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> list() {
+        return service.list();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JornadaTrabalho> get(@PathVariable("id") Long id)  {
+        try {
+            return ResponseEntity.ok(service.get(id));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping
+    public JornadaTrabalho update(@RequestBody JornadaTrabalho jornada) {
+        return service.update(jornada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.ok("Removido com sucesso");
     }
 }
--- src/main/java/dev/takasaki/becas/fistProject/service/JornadaTrabalhoService.java
+++ src/main/java/dev/takasaki/becas/fistProject/service/JornadaTrabalhoService.java
@@ -5,6 +5,9 @@ import dev.takasaki.becas.fistProject.repository.JornadaTrabalhoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 public class JornadaTrabalhoService {
     private final JornadaTrabalhoRepository jornadaTrabalhoRepository;
@@ -16,5 +19,23 @@ public class JornadaTrabalhoService {
 
     public JornadaTrabalho save(JornadaTrabalho jornadaTrabalho) {
         return jornadaTrabalhoRepository.save(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> list() {
+        return jornadaTrabalhoRepository.findAll();
+    }
+
+    public JornadaTrabalho get(Long id) throws NoSuchElementException {
+        return jornadaTrabalhoRepository
+                .findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Jornada não encontrada"));
+    }
+
+    public JornadaTrabalho update(JornadaTrabalho jornadaTrabalho) {
+        return jornadaTrabalhoRepository.save(jornadaTrabalho);
+    }
+
+    public void delete(Long id) {
+        jornadaTrabalhoRepository.deleteById(id);
     }
 }
