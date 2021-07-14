7616ff0a9da67cbdf669f0697e5dcf76997d8995
Takasakiii <lucasmc2709@live.com>
2021-07-14 14:47:29 -0300

    Finalização do projeto

--- src/main/java/dev/takasaki/becas/fistProject/model/JornadaTrabalho.java
+++ src/main/java/dev/takasaki/becas/fistProject/model/JornadaTrabalho.java
@@ -1,17 +1,21 @@
 package dev.takasaki.becas.fistProject.model;
 
 import lombok.Data;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @Entity
+@Audited
 public class JornadaTrabalho {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String descricao;
 }
